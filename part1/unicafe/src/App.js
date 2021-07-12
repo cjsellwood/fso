c330b3b7f958cd397740bff75931b459276fb739
cjsellwood <c.sellwood22@gmail.com>
2021-07-12 16:17:45 +1000

    1.6 complete

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const addGood = () => {
+    setGood(good + 1);
+  };
+
+  const addNeutral = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const addBad = () => {
+    setBad(bad + 1);
+  };
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={addGood} text="Good" />
+      <Button handleClick={addNeutral} text="Neutral" />
+      <Button handleClick={addBad} text="Bad" />
+
+      <h2>statistics</h2>
+      <div>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
