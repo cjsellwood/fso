c4477b29dd9d67a5028506b4834d91d5f4b76b5d
cjsellwood <c.sellwood22@gmail.com>
2021-07-12 16:22:43 +1000

    1.7 complete

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -34,6 +34,11 @@ const App = () => {
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
+        <p>all {good + neutral + bad}</p>
+        <p>
+          average {(good + neutral * 0 + bad * -1) / (good + neutral + bad)}
+        </p>
+        <p>positive {(100 * good) / (good + neutral + bad)} %</p>
       </div>
     </div>
   );
