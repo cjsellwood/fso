ce43f14ca88eaf8d53d6f111373b1d9ef219254d
cjsellwood <c.sellwood22@gmail.com>
2021-07-13 14:12:13 +1000

    2.9 complete

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [search, setSearch] = useState("");
 
   const changeName = (e) => {
     setNewName(e.target.value);
@@ -11,6 +17,10 @@ const App = () => {
 
   const changeNumber = (e) => {
     setNewNumber(e.target.value);
+  };
+
+  const changeSearch = (e) => {
+    setSearch(e.target.value);
   };
 
   const submitName = (e) => {
@@ -28,24 +38,30 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={search} onChange={changeSearch} />
+      </div>
+      <h3>Add a new</h3>
       <form onSubmit={submitName}>
         <div>
           name: <input value={newName} onChange={changeName} />
         </div>
         <div>
-          name: <input value={newNumber} onChange={changeNumber} />
+          number: <input value={newNumber} onChange={changeNumber} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
+      <h3>Numbers</h3>
       <ul>
-        {persons.map((person) => (
-          <li key={person.name}>
-            {person.name} {person.number}
-          </li>
-        ))}
+        {persons
+          .filter((person) => person.name.includes(search))
+          .map((person) => (
+            <li key={person.name}>
+              {person.name} {person.number}
+            </li>
+          ))}
       </ul>
     </div>
   );
