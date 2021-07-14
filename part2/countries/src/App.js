d3d2bec5fa861916d1704943231eb63c3f0d5afc
cjsellwood <c.sellwood22@gmail.com>
2021-07-14 12:53:53 +1000

    2.12 complete

---
+++ part2/countries/src/App.js
@@ -0,0 +1,60 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
+const App = () => {
+  const [search, setSearch] = useState("");
+  const [countries, setCountries] = useState({});
+  const [filterCountries, setFilterCountries] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://restcountries.eu/rest/v2/all").then((response) => {
+      setCountries(response.data);
+    });
+  }, []);
+
+  const searchInput = (e) => {
+    setSearch(e.target.value);
+    setFilterCountries(
+      countries.filter((country) =>
+        country.name.toLowerCase().includes(search.toLowerCase())
+      )
+    );
+  };
+
+  return (
+    <div>
+      <form>
+        <label>find countries</label>
+        <input value={search} onChange={searchInput} />
+      </form>
+      {filterCountries.length > 10 ? (
+        <p>Too many matches, specify another filter</p>
+      ) : filterCountries.length === 1 ? (
+        <div>
+          <h1>{filterCountries[0].name}</h1>
+          <p>capital {filterCountries[0].capital}</p>
+          <p>population {filterCountries[0].population}</p>
+          <h2>Languages</h2>
+          <ul>
+            {filterCountries[0].languages.map((language) => (
+              <li key={language.name}>{language.name}</li>
+            ))}
+          </ul>
+          <img
+            width="300px"
+            src={filterCountries[0].flag}
+            alt={filterCountries[0].name + " flag"}
+          />
+        </div>
+      ) : (
+        <ul>
+          {filterCountries.map((country) => (
+            <li key={country.name}>{country.name}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default App;
