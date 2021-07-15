5f4714ba15fe69ae9c9bf8dd00ac8a8c7f582ec6
cjsellwood <c.sellwood22@gmail.com>
2021-07-15 11:05:03 +1000

    2.18 complete

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -31,8 +31,25 @@ const App = () => {
   const submitEntry = (e) => {
     e.preventDefault();
 
-    if (persons.find((person) => person.name === newName)) {
-      alert(`${newName} is already in the phonebook`);
+    const existingPerson = persons.find((person) => person.name === newName);
+    if (existingPerson) {
+      if (
+        window.confirm(
+          `${newName} is already in the phonebook, replace the old number with a new one`
+        )
+      ) {
+        return personsService
+          .update(existingPerson.id, { name: newName, number: newNumber })
+          .then((updatedPerson) => {
+            setPersons(
+              persons.map((person) =>
+                person.id !== existingPerson.id ? person : updatedPerson
+              )
+            );
+            setNewName("");
+            setNewNumber("");
+          });
+      }
       return;
     }
 
