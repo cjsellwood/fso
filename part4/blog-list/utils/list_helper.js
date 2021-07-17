eed6b00788309f66e3fdadf3cffe6310984a2b54
cjsellwood <c.sellwood22@gmail.com>
2021-07-17 11:03:01 +1000

    4.4 complete

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,7 +1,14 @@
 const dummy = (blogs) => {
-  return 1
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((acc, cur) => {
+    return acc + cur.likes;
+  }, 0);
 };
 
 module.exports = {
   dummy,
+  totalLikes,
 };
