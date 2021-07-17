5e8b652ade918a89b9d2b95adafd5115c89ff2c6
cjsellwood <c.sellwood22@gmail.com>
2021-07-17 10:26:00 +1000

    4.2 complete

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,19 +1,12 @@
-const http = require("http");
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
+const blogRouter = require("./controllers/blog");
 
-const Blog = mongoose.model("Blog", blogSchema);
-
-const mongoUrl = "mongodb://localhost/blog-list";
+const mongoUrl = process.env.MONGODB_URI;
 mongoose.connect(mongoUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -24,21 +17,9 @@ mongoose.connect(mongoUrl, {
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
+app.use("/api/blogs", blogRouter);
 
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
-const PORT = 3003;
+const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
