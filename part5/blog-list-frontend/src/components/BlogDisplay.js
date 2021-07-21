21a4d732c5083db5f8b661ca93e606baa6f8afa0
cjsellwood <c.sellwood22@gmail.com>
2021-07-21 11:13:55 +1000

    5.9 complete

--- part5/blog-list-frontend/src/components/BlogDisplay.js
+++ part5/blog-list-frontend/src/components/BlogDisplay.js
@@ -3,9 +3,11 @@ import Blog from "./Blog";
 const BlogDisplay = ({ blogs, likeBlog }) => {
   return (
     <div>
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} likeBlog={likeBlog} />
-      ))}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} likeBlog={likeBlog} />
+        ))}
     </div>
   );
 };
