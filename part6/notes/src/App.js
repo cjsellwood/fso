de69f5456e0f742f83aa0fa3333139f22915c867
cjsellwood <c.sellwood22@gmail.com>
2021-07-23 11:44:37 +1000

    6.9 complete

--- part6/notes/src/App.js
+++ part6/notes/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Notes from "./components/Notes";
 import NewNote from "./components/newNote";
+import VisibilityFilter from "./components/VisibilityFilter";
 
 const App = () => {
   return (
     <div>
       <NewNote />
+      <VisibilityFilter />
       <Notes />
     </div>
   );
