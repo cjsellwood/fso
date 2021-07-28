7c29785b52d23d0e9d209aae1ceef5a5e7c3e0dd
cjsellwood <c.sellwood22@gmail.com>
2021-07-28 12:08:30 +1000

    8.7 complete

--- part8/library/app.js
+++ part8/library/app.js
@@ -115,6 +115,7 @@ const typeDefs = gql`
       published: Int!
       genres: [String!]!
     ): Book
+    editAuthor(name: String!, setBornTo: Int!): Author
   }
 `;
 
@@ -157,6 +158,19 @@ const resolvers = {
       const book = { ...args, id: uuid() };
       books = [...books, book];
       return book;
+    },
+    editAuthor: (root, args) => {
+      console.log(root, args);
+      const author = authors.find((author) => (author.name === args.name));
+      if (!author) {
+        return null;
+      }
+
+      const updatedAuthor = { ...author, born: args.setBornTo };
+      authors = authors.map((author) =>
+        author.name == args.name ? updatedAuthor : author
+      );
+      return updatedAuthor;
     },
   },
 };
