f797ed2eba7f39e6a3ec832cba419b4f544f17bb
cjsellwood <c.sellwood22@gmail.com>
2021-07-30 13:26:41 +1000

    login and updating the cache example

--- part8/phonebook-frontend/src/App.js
+++ part8/phonebook-frontend/src/App.js
@@ -1,17 +1,26 @@
 import React, { useState } from "react";
-import { useQuery } from "@apollo/client";
+import { useApolloClient, useQuery } from "@apollo/client";
 import Persons from "./components/Persons";
 import PersonsForm from "./components/PersonForm";
 import { ALL_PERSONS } from "./queries";
 import PhoneForm from "./components/PhoneForm";
+import LoginForm from "./components/LoginForm";
 
 function App() {
+  const [token, setToken] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
   const result = useQuery(ALL_PERSONS);
+  const client = useApolloClient();
 
   if (result.loading) {
     return <div>loading...</div>;
   }
+
+  const logout = () => {
+    setToken(null);
+    localStorage.clear();
+    client.resetStore();
+  };
 
   const notify = (message) => {
     setErrorMessage(message);
@@ -19,12 +28,24 @@ function App() {
       setErrorMessage(null);
     }, 10000);
   };
+
+  if (!token) {
+    return (
+      <div>
+        <Notify errorMessage={errorMessage} />
+        <h2>Login</h2>
+        <LoginForm setToken={setToken} setError={notify} />
+      </div>
+    );
+  }
+
   return (
     <div>
+      <button onClick={logout}>logout</button>
       <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons} />
       <PersonsForm setError={notify} />
-      <PhoneForm setError={notify}/>
+      <PhoneForm setError={notify} />
     </div>
   );
 }
--- part8/phonebook-frontend/src/index.js
+++ part8/phonebook-frontend/src/index.js
@@ -7,32 +7,25 @@ import {
   ApolloProvider,
   HttpLink,
   InMemoryCache,
-  gql,
 } from "@apollo/client";
+
+import { setContext } from "apollo-link-context";
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("phonenumbers-user-token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `bearer ${token}` : null,
+    },
+  };
+});
+
+const httpLink = new HttpLink({ uri: "http://localhost:4000" });
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: "http://localhost:4000",
-  }),
-});
-
-const query = gql`
-  query {
-    allPersons {
-      name
-      phone
-      address {
-        street
-        city
-      }
-      id
-    }
-  }
-`;
-
-client.query({ query }).then((response) => {
-  console.log(response.data);
+  link: authLink.concat(httpLink),
 });
 
 ReactDOM.render(
