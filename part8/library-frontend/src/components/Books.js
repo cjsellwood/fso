663392fa8c2e8d2fc6ed830b9d4b66bc981b8271
cjsellwood <c.sellwood22@gmail.com>
2021-07-30 14:13:03 +1000

    8.19 complete

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -4,11 +4,23 @@ import { ALL_BOOKS } from "../queries";
 
 const Books = (props) => {
   const [books, setBooks] = useState([]);
+  const [genres, setGenres] = useState([]);
+  const [genreFilter, setGenreFilter] = useState(null);
   const result = useQuery(ALL_BOOKS);
 
   useEffect(() => {
     if (result.data) {
       setBooks(result.data.allBooks);
+      const newGenres = [];
+      result.data.allBooks.forEach((book) => {
+        book.genres.forEach((genre) => {
+          if (!newGenres.includes(genre)) {
+            newGenres.push(genre);
+          }
+        });
+      });
+      console.log(newGenres);
+      setGenres(newGenres);
     }
   }, [result]);
 
@@ -19,7 +31,6 @@ const Books = (props) => {
   return (
     <div>
       <h2>books</h2>
-
       <table>
         <tbody>
           <tr>
@@ -27,15 +38,34 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {books
+            .filter((book) => {
+              if (genreFilter) {
+                return book.genres.includes(genreFilter);
+              }
+              return book;
+            })
+            .map((a) => (
+              <tr key={a.title}>
+                <td>{a.title}</td>
+                <td>{a.author.name}</td>
+                <td>{a.published}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
+      <div>
+        {genres.map((genre) => {
+          return (
+            <button key={genre} onClick={() => setGenreFilter(genre)}>
+              {genre}
+            </button>
+          );
+        })}
+        <button onClick={() => setGenreFilter(null)}>
+              All Genres
+            </button>
+      </div>
     </div>
   );
 };
