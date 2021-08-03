cef6d1fe405a0fc11a51e9861ff13c89ee66a3a8
cjsellwood <c.sellwood22@gmail.com>
2021-08-03 14:53:56 +1000

    9.19 complete

--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -4,9 +4,6 @@ export interface Diagnosis {
   latin?: string;
 }
 
-//eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {}
-
 export interface Patient {
   id: string;
   name: string;
@@ -28,3 +25,49 @@ export type NonSensitivePatient = Omit<Patient, "ssn">;
 export type NewPatient = Omit<Patient, "id">;
 
 export type PublicPatient = Omit<Patient, "ssn" | "entries">;
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis["code"]>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface OccupationHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationHealthcareEntry
+  | HealthCheckEntry;
